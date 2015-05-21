e260fe3ac56a6d4578ceb86a8ad2d56d9cff02f9
Megan Dunbar <meganleedunbar@gmail.com>
2015-05-21 14:31:32 -0400

    updated JS to pull correct API

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,29 @@
 (function(){
   'use strict';
 
-  var url = 'https://api.etsy.com/v2/listings/active?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=whiskey&includes=Images,Shop&sort_on=score';
-  fetchJSON(url, app);
+/*Then you can call the function like this,
+where you would replace logResults with the name
+of the function you want to be called with the results. */
+
+
+
+  var url = "https://api.etsy.com/v2/listings/active?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
+fetchJSONP(url, logResults);
 
 //copied code to fetch the data from the above place...where JSON lives
 
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
 
-function fetchJSON(url, callback){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            callback(JSON.parse(xmlhttp.responseText));
-        }
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
     };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
 }
 
 };
