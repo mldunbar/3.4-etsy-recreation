09ac695e3d77e89d2272baa5cc3fa798e18d4952
Megan Dunbar <meganleedunbar@gmail.com>
2015-05-21 14:20:57 -0400

    HTML added for header

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,21 @@
-alert("hello")
+(function(){
+  'use strict';
+
+  var url = 'https://api.etsy.com/v2/listings/active?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=whiskey&includes=Images,Shop&sort_on=score';
+  fetchJSON(url, app);
+
+//copied code to fetch the data from the above place...where JSON lives
+
+
+function fetchJSON(url, callback){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(JSON.parse(xmlhttp.responseText));
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+};
